def219d8edce58d7c669ebd98810c01aa258c1f0
Samuel Meenzen <samuel@meenzen.net>
2024-02-27 17:26:38 +0100

    feat: add autorized keys

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -86,8 +86,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
--- nixos/users.nix
+++ nixos/users.nix
@@ -8,7 +8,8 @@
       initialPassword = "password123";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMa9vjZasAelcVAdtLa+vI0dYvx4hba2z6z+J+u39irB meenzens@framework"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDOHTWbt687mGfFsdxrgSyCtyrb547mw5+SL3FdAT5KeAAAABHNzaDo= YubiKey C"
       ];
       description = "Samuel Meenzen";
       extraGroups = [ "networkmanager" "wheel" "docker" ];
