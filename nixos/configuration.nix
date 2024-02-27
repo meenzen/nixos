c8a007680713570e27b4652a5aa995e45d0d347b
Samuel Meenzen <samuel@meenzen.net>
2024-02-27 13:50:06 +0100

    feat: add fonts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -165,6 +165,45 @@
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "meenzens";
 
+  # Fonts
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+    };
+    aggregatedIcons = pkgs.buildEnv {
+      name = "system-icons";
+      paths = with pkgs; [
+        libsForQt5.breeze-qt5  # for plasma
+        #gnome.gnome-themes-extra
+      ];
+      pathsToLink = [ "/share/icons" ];
+    };
+    aggregatedFonts = pkgs.buildEnv {
+      name = "system-fonts";
+      paths = config.fonts.packages;
+      pathsToLink = [ "/share/fonts" ];
+    };
+  in {
+    "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+    "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" "Hack" "JetBrainsMono" ]; })
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      # microsoft fonts
+      corefonts
+      vistafonts
+    ];
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
