761fc436701e38d093c6346dd8c83d1f1babb4cb
Samuel Meenzen <samuel@meenzen.net>
2024-02-27 14:03:06 +0100

    chore: update wezterm config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -172,6 +172,8 @@
     extraConfig = ''
       return {
         font = wezterm.font_with_fallback { 'Hack Nerd Font', 'Noto Color Emoji' },
+        font_size = 16.0,
+        hide_tab_bar_if_only_one_tab = true,
       }
     '';
   };
