69712ca8bdb0cf1de60f269c6d2db814e1b36ef2
Samuel Meenzen <samuel@meenzen.net>
2024-03-09 22:49:43 +0100

    feat: improved scripts

--- update.sh
+++ update.sh
@@ -2,7 +2,22 @@
 
 set -eo pipefail
 
+LIGHT_BLUE='\033[1;34m'
+NO_COLOR='\033[0m'
+
+print () {
+    echo -e "${LIGHT_BLUE}$1${NO_COLOR}"
+}
+
+print "=> Starrting Update"
+
+print "==> Formatting Code"
+alejandra .
+
+print "==> Updating Flake"
 git add .
 nix flake update --verbose
+
+print "==> Rebuilding NixOS"
 git add .
 sudo nixos-rebuild switch --upgrade --verbose --flake .
