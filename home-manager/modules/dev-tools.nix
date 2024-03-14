12c8426f341e77c1e24359c57249428e211a8747
Samuel Meenzen <samuel@meenzen.net>
2024-03-14 10:34:40 +0100

    fix: dotnet workload support

--- home-manager/modules/dev-tools.nix
+++ home-manager/modules/dev-tools.nix
@@ -1,4 +1,33 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  dotnet-combined =
+    (with pkgs.dotnetCorePackages;
+      combinePackages [
+        sdk_8_0
+        sdk_7_0
+        sdk_6_0
+      ])
+    .overrideAttrs (finalAttrs: previousAttrs: {
+      # This is needed to install workload in $HOME
+      # https://discourse.nixos.org/t/dotnet-maui-workload/20370/12
+
+      postBuild =
+        (previousAttrs.postBuild or '''')
+        + ''
+           for i in $out/sdk/*
+           do
+             i=$(basename $i)
+             length=$(printf "%s" "$i" | wc -c)
+             substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+             i="$substring""00"
+             mkdir -p $out/metadata/workloads/''${i/-*}
+             touch $out/metadata/workloads/''${i/-*}/userlocal
+          done
+        '';
+    });
+in {
+  home.sessionVariables = {
+    DOTNET_ROOT = "${dotnet-combined}";
+  };
   home.packages = with pkgs; [
     vscode
     kate
@@ -6,11 +35,6 @@
     rustup
     tokei
     glow
-    (with dotnetCorePackages;
-      combinePackages [
-        sdk_8_0
-        sdk_7_0
-        sdk_6_0
-      ])
+    dotnet-combined
   ];
 }
