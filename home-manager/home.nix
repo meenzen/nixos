034825823a5e2fb429ed60b53d00dd27adc63518
Samuel Meenzen <samuel@meenzen.net>
2024-03-17 18:57:13 +0100

    feat: tokei fork

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -39,6 +39,23 @@
       #     patches = [ ./change-hello-to-hi.patch ];
       #   });
       # })
+
+      # tokei: add support for razor files, see https://github.com/XAMPPRocky/tokei/pull/992
+      (final: prev: {
+        tokei = prev.tokei.overrideAttrs (oldAttrs: rec {
+          version = "latest-meenzen";
+          src = prev.fetchFromGitHub {
+            owner = "meenzen";
+            repo = "tokei";
+            rev = "d1845f54a9bbc0625e83f06556ebab44da9247c8";
+            sha256 = "sha256-vQa0ZUxeD3Wj2PH7FLT/GLYl71xQKfKw7rPGS6Lk2JA=";
+          };
+          cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
+            inherit src;
+            outputHash = "sha256-QuXCl599e/dSuvC/U+eq95xtJnGvbVXYBHLwWgYIhck=";
+          });
+        });
+      })
     ];
     # Configure your nixpkgs instance
     config = {
