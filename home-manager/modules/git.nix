7e3b88cbf18f6a1abfd01ccc506aa36e9fe34118
Samuel Meenzen <samuel@meenzen.net>
2024-03-17 16:25:08 +0100

    feat: git config

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,15 +1,21 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
     userName = "Samuel Meenzen";
     userEmail = "samuel@meenzen.net";
+    lfs.enable = true;
     extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      rerere = {
-        enabled = true;
-      };
+      init.defaultBranch = "main";
+      credential.helper = "libsecret";
+      rerere.enabled = true;
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "/home/${config.home.username}/.ssh/id_ed25519_sk";
     };
   };
 }
