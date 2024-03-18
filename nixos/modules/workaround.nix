16ffb32f847d4d420356a8e4c77df99ea4845f51
Samuel Meenzen <samuel@meenzen.net>
2024-03-18 14:43:31 +0100

    fix: dll workaround

--- nixos/modules/workaround.nix
+++ nixos/modules/workaround.nix
@@ -19,4 +19,17 @@
       value.source = value.flake;
     })
     config.nix.registry;
+
+  # https://nix.dev/guides/faq.html#how-to-run-non-nix-executables
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
+  environment.variables = {
+    LD_LIBRARY_PATH = with pkgs;
+      lib.makeLibraryPath [
+        fontconfig
+      ];
+  };
 }
