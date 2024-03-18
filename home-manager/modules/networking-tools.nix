23734e76b6f5eb945640fbe7699b5c949e5b2638
Samuel Meenzen <samuel@meenzen.net>
2024-03-18 19:14:12 +0100

    feat: add speedtest

--- home-manager/modules/networking-tools.nix
+++ home-manager/modules/networking-tools.nix
@@ -7,5 +7,6 @@
     aria2 # A lightweight multi-protocol & multi-source command-line download utility
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
+    ookla-speedtest # provides the `speedtest` command
   ];
 }
