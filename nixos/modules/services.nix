45f77a6e4e98347d6bff756276d2552d6b71c673
Samuel Meenzen <samuel@meenzen.net>
2024-03-19 16:25:52 +0100

    fix: docker config

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -8,7 +8,57 @@
   };
 
   # Docker
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      # use a mirror that is not rate limited
+      "registry-mirrors" = ["https://mirror.gcr.io"];
+
+      # custom address pools to avoid conflicts with the corporate network
+      "default-address-pools" = [
+        {
+          base = "192.168.180.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.181.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.182.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.183.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.184.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.185.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.186.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.187.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.188.0/24";
+          size = 24;
+        }
+        {
+          base = "192.168.189.0/24";
+          size = 24;
+        }
+      ];
+    };
+  };
 
   # udev rules
   services.udev.packages = with pkgs; [
