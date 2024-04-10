9add450a649267be14a42df20cee8c118c17e39b
Samuel Meenzen <samuel@meenzen.net>
2024-04-10 12:26:58 +0200

    feat: JetBrains IDEs with plugins

--- home-manager/modules/dev-tools.nix
+++ home-manager/modules/dev-tools.nix
@@ -24,6 +24,8 @@
           done
         '';
     });
+
+  jetbrains-plugins = ["github-copilot" "ideavim"];
 in {
   home.sessionVariables = {
     DOTNET_ROOT = "${dotnet-combined}";
@@ -40,12 +42,12 @@ in {
     vscode
     kdePackages.kate
 
-    # JetBrains IDEs don't work correctly, plugins are broken.
-    #jetbrains.rider
-    #jetbrains.rust-rover
-    #jetbrains.idea-ultimate
-    #jetbrains.webstorm
-    jetbrains-toolbox # install IDEs from toolbox manually for now
+    # https://nixos.wiki/wiki/Jetbrains_Tools
+    (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.rider jetbrains-plugins)
+    (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.rust-rover jetbrains-plugins)
+    (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.idea-ultimate jetbrains-plugins)
+    (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.webstorm jetbrains-plugins)
+    #jetbrains-toolbox
 
     # Rust
     rustc
@@ -57,4 +59,46 @@ in {
     # Misc
     hyperfine
   ];
+
+  home.file.".ideavimrc".text = ''
+    " .ideavimrc is a configuration file for IdeaVim plugin. It uses
+    "   the same commands as the original .vimrc configuration.
+    " You can find a list of commands here: https://jb.gg/h38q75
+    " Find more examples here: https://jb.gg/share-ideavimrc
+
+    "" -- Suggested options --
+    " Show a few lines of context around the cursor. Note that this makes the
+    " text scroll if you mouse-click near the start or end of the window.
+    set scrolloff=5
+
+    " Do incremental searching.
+    set incsearch
+
+    " J - Join things.
+    set ideajoin
+
+    " Don't use Ex mode, use Q for formatting.
+    map Q gq
+
+    " --- Enable IdeaVim plugins https://jb.gg/ideavim-plugins
+
+    " Highlight copied text
+    Plug 'machakann/vim-highlightedyank'
+    " Commentary plugin
+    Plug 'tpope/vim-commentary'
+
+    " Quickscope plugin (install IdeaVim-Quickscope in Settings | Plugins)
+    set quickscope
+    let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
+
+    "" -- Map IDE actions to IdeaVim -- https://jb.gg/abva4t
+    "" Map \r to the Reformat Code action
+    map \r <Action>(ReformatCode)
+
+    "" Map <leader>d to start debug
+    "map <leader>d <Action>(Debug)
+
+    "" Map \b to toggle the breakpoint on the current line
+    map \b <Action>(ToggleLineBreakpoint)
+  '';
 }
