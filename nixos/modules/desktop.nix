7c681b566567722f53f3ba271ad9badb8d5e3ba9
Samuel Meenzen <samuel@meenzen.net>
2024-04-12 10:11:54 +0200

    chore: update SDDM

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -21,8 +21,8 @@
   services.printing.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.displayManager.sddm.wayland.enable = true; # SDDM Wayland support is still a little unstable
+  services.displayManager.sddm.enable = true;
+  #services.displayManager.sddm.wayland.enable = true; # SDDM Wayland support is still a little unstable
   services.desktopManager.plasma6.enable = true;
   services.desktopManager.plasma6.enableQt5Integration = true;
 
