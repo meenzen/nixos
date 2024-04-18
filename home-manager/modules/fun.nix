b5ed250f04c8c91b0da7e6a159ed6dfcebe11b3e
Samuel Meenzen <samuel@meenzen.net>
2024-04-18 11:24:16 +0200

    feat: add some more fun commands

--- home-manager/modules/fun.nix
+++ home-manager/modules/fun.nix
@@ -1,9 +1,14 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     cowsay
+    ponysay
     fastfetch
     asciiquarium
     clolcat
+    cmatrix
+    fortune
+    sl
+    bb
   ];
 
   programs.thefuck.enable = true;
