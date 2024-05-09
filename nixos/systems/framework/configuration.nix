0c4e0eea36f76d166715c66ed4b2eeef878936ea
Samuel Meenzen <samuel@meenzen.net>
2024-05-09 14:32:51 +0200

    chore: update

--- nixos/systems/framework/configuration.nix
+++ nixos/systems/framework/configuration.nix
@@ -35,14 +35,14 @@
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://crane.cachix.org"
-      "https://nix.computer.surgery/conduit"
+      "https://attic.conduit.rs/conduit"
       "https://cache.nixos.org/"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
-      "conduit:ZGAf6P6LhNvnoJJ3Me3PRg7tlLSrPxcQ2RiE5LIppjo="
+      "conduit:ddcaWZiWm0l0IXZlO8FERRdWvEufwmd0Negl1P+c0Ns="
     ];
   };
 
