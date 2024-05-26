7aa1a8f0f8c05232e4b9db0ce209acef769ca4ad
Samuel Meenzen <samuel@meenzen.net>
2024-05-26 19:34:59 +0200

    feat: mvidia driver beta from nixpkgs

--- nixos/hardware/nvidia/default.nix
+++ nixos/hardware/nvidia/default.nix
@@ -17,15 +17,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    #config.boot.kernelPackages.nvidiaPackages.latest
-    #config.boot.kernelPackages.nvidiaPackages.beta # https://nixpk.gs/pr-tracker.html?pr=313440
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.42.02";
-      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-      sha256_aarch64 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      persistencedSha256 = lib.fakeSha256;
-    };
+    #package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.beta; # 555 beta (explicit sync for proper wayland support)
   };
 }
