acac0b15ec5ef2eb92a810d958ea6822afdacefe
Samuel Meenzen <samuel@meenzen.net>
2024-05-31 09:52:29 +0200

    feat: add datagrip

--- home-manager/modules/dev-tools.nix
+++ home-manager/modules/dev-tools.nix
@@ -60,6 +60,7 @@ in {
     (jetbrains.plugins.addPlugins jetbrains.idea-ultimate jetbrains-plugins)
     (jetbrains.plugins.addPlugins jetbrains.webstorm jetbrains-plugins)
     (jetbrains.plugins.addPlugins jetbrains.phpstorm jetbrains-plugins)
+    (jetbrains.plugins.addPlugins jetbrains.datagrip jetbrains-plugins)
     #jetbrains-toolbox
 
     # Rust
