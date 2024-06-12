45b71540e7863b5255584823caae3d423ecdadcb
Samuel Meenzen <samuel@meenzen.net>
2024-06-12 16:43:34 +0200

    feat: amd ryzen config

--- nixos/systems/the-machine/configuration.nix
+++ nixos/systems/the-machine/configuration.nix
@@ -7,7 +7,9 @@
 }: {
   imports = [
     inputs.nixos-hardware.nixosModules.common-pc-ssd
-    inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
 
     ../../modules
