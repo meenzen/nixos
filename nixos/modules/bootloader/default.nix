db8bd0240bcc92e1f99961803dca654fc7f7c6e3
Samuel Meenzen <samuel@meenzen.net>
2024-06-12 20:22:19 +0200

    feat: add memtest86

--- nixos/modules/bootloader/default.nix
+++ nixos/modules/bootloader/default.nix
@@ -2,5 +2,6 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 20;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
