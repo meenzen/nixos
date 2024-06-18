a192b6797136279296832ef25a54214ab57535eb
Samuel Meenzen <samuel@meenzen.net>
2024-06-18 13:53:19 +0200

    chore: update

--- flake.nix
+++ flake.nix
@@ -18,8 +18,7 @@
     stylix.url = "github:danth/stylix";
     nixvim = {
       url = "github:nix-community/nixvim";
-      # temporary workaround https://github.com/nix-community/nixvim/issues/1699
-      #inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Gaming
