ebdc519449669fb23a08114031ed2f683bcbf12c
Samuel Meenzen <samuel@meenzen.net>
2024-07-11 20:37:05 +0200

    fix: update nvidia

--- nixos/hardware/nvidia/default.nix
+++ nixos/hardware/nvidia/default.nix
@@ -4,12 +4,17 @@
   lib,
   ...
 }: {
-  # Nvidia OpenGL
-  hardware.opengl.extraPackages = with pkgs; [vaapiVdpau];
+  # Nvidia
+  hardware.graphics.extraPackages = with pkgs; [vaapiVdpau];
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
-  boot.kernelParams = ["nvidia-drm.modeset=1" "nvidia-drm.fbdev=1"];
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+    # workaround for 555 stutters
+    "nvidia.NVreg_EnableGpuFirmware=0"
+  ];
 
   hardware.nvidia = {
     modesetting.enable = true;
