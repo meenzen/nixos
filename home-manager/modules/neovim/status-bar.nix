86fae8a4d2a1ac27707d492007c52fe381d9cded
Samuel Meenzen <samuel@meenzen.net>
2024-09-20 12:46:59 +0200

    chore: rename lualine options

--- home-manager/modules/neovim/status-bar.nix
+++ home-manager/modules/neovim/status-bar.nix
@@ -2,10 +2,12 @@
   programs.nixvim.plugins = {
     lualine = {
       enable = true;
-      globalstatus = true;
-      extensions = [
-        "fzf"
-      ];
+      settings = {
+        extensions = [
+          "fzf"
+        ];
+        options.globalstatus = true;
+      };
     };
     navic.enable = true; # lualine code structure
     bufferline.enable = true;
