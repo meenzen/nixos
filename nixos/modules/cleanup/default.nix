e0521ea5a492b1c5a9e8b6b6449ff5b11abe770a
Samuel Meenzen <samuel@meenzen.net>
2024-11-14 13:58:17 +0100

    feat: add cleanup script

--- nixos/modules/cleanup/default.nix
+++ nixos/modules/cleanup/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   # Automatic garbage collection
   nix.gc = {
     automatic = true;
@@ -8,4 +8,16 @@
 
   # Deduplicate and optimize nix store
   nix.settings.auto-optimise-store = true;
+
+  environment.systemPackages = [
+    (
+      pkgs.writeScriptBin "cleanup-nix-store" ''
+        set -eux
+
+        sudo nix-collect-garbage --delete-older-than 7d
+        sudo nix-store --optimise
+        sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +2
+      ''
+    )
+  ];
 }
