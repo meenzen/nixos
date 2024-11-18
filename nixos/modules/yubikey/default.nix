0f71e69098adcccc508cab95b5a2485d05737598
Samuel Meenzen <samuel@meenzen.net>
2024-11-18 09:49:29 +0100

    feat: improved yubikey support

--- nixos/modules/yubikey/default.nix
+++ nixos/modules/yubikey/default.nix
@@ -1,5 +1,14 @@
 {pkgs, ...}: {
+  # YubiKey support
   services.udev.packages = with pkgs; [
     yubikey-personalization
+  ];
+
+  # Smartcard support
+  services.pcscd.enable = true;
+
+  # Management GUI
+  environment.systemPackages = with pkgs; [
+    yubioath-flutter
   ];
 }
