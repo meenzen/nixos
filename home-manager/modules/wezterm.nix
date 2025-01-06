a78b11b55d3710a98ebfc3eabf6914bab1360af2
Samuel Meenzen <samuel@meenzen.net>
2025-01-06 10:34:15 +0100

    chore: remove wezterm workaround

--- home-manager/modules/wezterm.nix
+++ home-manager/modules/wezterm.nix
@@ -4,10 +4,6 @@
     extraConfig = ''
       return {
         hide_tab_bar_if_only_one_tab = true,
-
-        -- Workaround for https://github.com/NixOS/nixpkgs/issues/336069
-        front_end = "WebGpu",
-        enable_wayland = false,
       }
     '';
   };
