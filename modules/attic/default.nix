ea54cfa2c0035cf603c0d6a0616cbd24ced93e1d
Samuel Meenzen <samuel@meenzen.net>
2025-01-11 13:34:08 +0100

    fix: increase client_max_body_size even more

--- modules/attic/default.nix
+++ modules/attic/default.nix
@@ -86,7 +86,7 @@ in {
         enableACME = true;
         locations."/".proxyPass = "http://127.0.0.1:${toString cfg.port}";
         extraConfig = ''
-          client_max_body_size 2g;
+          client_max_body_size 3g;
         '';
       };
     };
