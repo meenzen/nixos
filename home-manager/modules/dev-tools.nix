1d7b93c1b85472b6353d5a20c71f77c499461220
Samuel Meenzen <samuel@meenzen.net>
2025-02-10 10:18:26 +0100

    fix: simplify dotnet setup to make workloads install correctly

--- home-manager/modules/dev-tools.nix
+++ home-manager/modules/dev-tools.nix
@@ -3,38 +3,15 @@
   inputs,
   ...
 }: let
-  dotnet-combined =
-    (with pkgs.dotnetCorePackages;
-      combinePackages [
-        sdk_9_0
-        sdk_8_0
-      ])
-    .overrideAttrs (finalAttrs: previousAttrs: {
-      # This is needed to install workload in $HOME
-      # https://discourse.nixos.org/t/dotnet-maui-workload/20370/12
-
-      postBuild =
-        (previousAttrs.postBuild or '''')
-        + ''
-          for i in $out/sdk/*
-          do
-            i=$(basename $i)
-            length=$(printf "%s" "$i" | wc -c)
-            substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
-            i="$substring""00"
-            mkdir -p $out/metadata/workloads/''${i/-*}
-            touch $out/metadata/workloads/''${i/-*}/userlocal
-          done
-        '';
-    });
+  dotnet = pkgs.dotnetCorePackages.sdk_9_0;
 in {
   home.sessionVariables = {
-    DOTNET_ROOT = "${dotnet-combined}/share/dotnet";
+    DOTNET_ROOT = "${dotnet}/share/dotnet";
     MSBUILDTERMINALLOGGER = "auto";
   };
   home.packages = [
     # Compilers
-    dotnet-combined
+    dotnet
     pkgs.gcc
 
     # Rust
