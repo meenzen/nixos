be8c9329502ce6e3855b7f2629d08f68c8be248d
Samuel Meenzen <samuel@meenzen.net>
2025-03-18 22:39:45 +0100

    feat: add more fun commands

--- home-manager/modules/fun.nix
+++ home-manager/modules/fun.nix
@@ -9,6 +9,15 @@
     pkgs.fortune
     pkgs.sl
     pkgs.bb
+
+    (pkgs.writeScriptBin "ai" ''
+      echo "AI is now enabled!"
+    '')
+    (pkgs.writeScriptBin "llm" ''
+      echo "Generating message..."
+      sleep 1
+      fortune | ponysay
+    '')
   ];
 
   programs.thefuck.enable = true;
