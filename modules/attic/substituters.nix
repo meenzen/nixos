52553c5806c5a2298648fe20e7ec57680c77eabe
Samuel Meenzen <samuel@meenzen.net>
2025-06-10 10:29:58 +0200

    feat: add another attic cache

--- modules/attic/substituters.nix
+++ modules/attic/substituters.nix
@@ -1,6 +1,12 @@
 {
   nix.settings = {
-    substituters = ["https://attic.mnzn.dev/main"];
-    trusted-public-keys = ["main:S8Ire6opydyGvlK4PI03t1UIwFsYJqKvn6EJrY4EoNA="];
+    substituters = [
+      "https://attic.mnzn.dev/main"
+      "https://attic.human-dev.io/human"
+    ];
+    trusted-public-keys = [
+      "main:S8Ire6opydyGvlK4PI03t1UIwFsYJqKvn6EJrY4EoNA="
+      "human:TFU+2BTCP6U2cZwUJ9iKGCJvsuRwe/alCqrN9TjgN/4="
+    ];
   };
 }
