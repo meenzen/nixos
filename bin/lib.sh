41d5617cbecc8df05503914d2d52861bc2fd06ab
Samuel Meenzen <samuel@meenzen.net>
2025-06-14 13:59:49 +0200

    refactor: simplify scripts

--- bin/lib.sh
+++ bin/lib.sh
@@ -6,6 +6,26 @@ print () {
   echo -e "${LIGHT_BLUE}$1${NO_COLOR}"
 }
 
+print_start () {
+  print "=> $1"
+}
+
+print_end () {
+  print "=> $1"
+}
+
+print_status () {
+  print "==> $1"
+}
+
 print_error () {
   echo -e "${RED}Error:${NO_COLOR} $1"
+}
+
+prompt_or_exit () {
+  echo "$1 [y/N]"
+  read -r response
+  if [ "$response" != "y" ]; then
+    exit 0
+  fi
 }
