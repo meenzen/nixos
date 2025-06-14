839028d5471b5f494af8d811ea1a96d50f811d63
Samuel Meenzen <samuel@meenzen.net>
2025-06-14 14:35:45 +0200

    refactor: simplify scripts

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,9 +1,9 @@
-LIGHT_BLUE='\033[1;34m'
-RED='\033[0;31m'
-NO_COLOR='\033[0m'
+COLOR_LIGHT_BLUE='\033[1;34m'
+COLOR_RED='\033[0;31m'
+COLOR_NONE='\033[0m'
 
 print () {
-  echo -e "${LIGHT_BLUE}$1${NO_COLOR}"
+  echo -e "${COLOR_LIGHT_BLUE}$1${COLOR_NONE}"
 }
 
 print_start () {
@@ -18,23 +18,27 @@ print_status () {
   print "==> $1"
 }
 
+print_warning () {
+  echo -e "Warning: ${COLOR_RED}$1${COLOR_NONE}"
+}
+
 print_error () {
-  echo -e "${RED}==> Error:${NO_COLOR} $1"
+  echo -e "${COLOR_RED}==> Error:${COLOR_NONE} $1"
 }
 
 print_divider () {
   local columns=${COLUMNS:-80}
   local divider_char="${1:-─}"
-  local color="${2:-$LIGHT_BLUE}"
+  local color="${2:-$COLOR_LIGHT_BLUE}"
   local divider_line=""
   for ((i=0; i<columns; i++)); do
     divider_line+="$divider_char"
   done
-  echo -e "${color}${divider_line}${NO_COLOR}"
+  echo -e "${color}${divider_line}${COLOR_NONE}"
 }
 
 print_divider_error () {
-  print_divider "" "$RED"
+  print_divider "" "$COLOR_RED"
 }
 
 prompt_or_exit () {
