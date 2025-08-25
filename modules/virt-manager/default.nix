3ac4cce0e2139ab0c14a12f2024a1d1f5dc48acf
Samuel Meenzen <samuel@meenzen.net>
2025-08-25 11:51:30 +0200

    feat: improved virt-manager config

--- modules/virt-manager/default.nix
+++ modules/virt-manager/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   systemConfig,
   ...
 }: let
@@ -12,8 +13,17 @@ in {
 
   config = lib.mkIf cfg.enable {
     programs.virt-manager.enable = true;
-    users.groups.libvirtd.members = [systemConfig.user.username];
-    virtualisation.libvirtd.enable = true;
+    users.groups = {
+      libvirtd.members = [systemConfig.user.username];
+      kvm.members = [systemConfig.user.username];
+    };
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
     virtualisation.spiceUSBRedirection.enable = true;
     networking.firewall.trustedInterfaces = [
       "virbr0"
