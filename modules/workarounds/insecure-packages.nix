e2704333c213147900ec1969876b939cbb1ba991
Samuel Meenzen <samuel@meenzen.net>
2025-08-31 22:38:18 +0200

    chore: remove insecure dotnet versions

--- modules/workarounds/insecure-packages.nix
+++ modules/workarounds/insecure-packages.nix
@@ -2,11 +2,5 @@
   nixpkgs.config.permittedInsecurePackages = [
     # Legacy olm library is required for some matrix clients
     "olm-3.2.16"
-
-    "dotnet-core-combined"
-    "dotnet-sdk-6.0.428"
-    "dotnet-sdk-7.0.410"
-    "dotnet-sdk-wrapped-6.0.428"
-    "dotnet-sdk-wrapped-7.0.410"
   ];
 }
