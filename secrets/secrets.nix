20493a8eedf94a0cf9611b54eb5c5fee15d82c9d
Samuel Meenzen <samuel@meenzen.net>
2025-09-11 23:23:56 +0200

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,7 @@ let
   users = [meenzens meenzens1];
 
   # ssh-keyscan -t ed25519 <host>
-  neon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsdZx97CPaUCYagHxZKuB9r9nfRjB2Rv4UWJtWyy+7e";
+  neon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdUsfwRNDJCe4DTStnmP6Se+Af8V5+Lqp1iSBSGWBKD";
   framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILK3+4hV7Nt878acFUZkA/+TP1/l3gBQ/k1EHY4V94IF";
   lithium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVJKyiSdl3cZh8r3sLYj2IaWHJ2opgBzGeFj7Vtzjng";
   systems = [neon lithium framework];
