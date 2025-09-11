dc3dd2a34dc46febccf92aca7876f0d9abf04605
Samuel Meenzen <samuel@meenzen.net>
2025-09-11 23:38:40 +0200

    feat: reenable all services

--- systems/neon/configuration.nix
+++ systems/neon/configuration.nix
@@ -22,33 +22,33 @@
   system.stateVersion = "24.11";
 
   meenzen.server.enable = true;
-  #meenzen.backup.enable = true;
+  meenzen.backup.enable = true;
   meenzen.hetzner.enable = true;
-  #meenzen.nginx = {
-  #  enable = true;
-  #  enableCloudflare = true;
-  #  testPage = "neon.mnzn.dev";
-  #};
-  #meenzen.grafana.enable = true;
-  #meenzen.promtail.enable = true;
-  #meenzen.postgresql.enable = true;
-  #meenzen.oci-containers.enable = true;
-  #meenzen.mastodon = {
-  #  enable = true;
-  #  enableSearch = true;
-  #};
-  #meenzen.fedifetcher.enable = true;
-  #meenzen.matrix.enable = true;
-  #meenzen.authentik.enable = true;
-  #meenzen.gitlab.enable = true;
-  #meenzen.collabora.enable = true;
-  #meenzen.cheshbot.enable = true;
-  #meenzen.attic.enable = true;
-  #meenzen.distributed-build.enableHost = true;
-  #meenzen.mudblazor-docs.enable = true;
-  #meenzen.websites."mnzn.dev".enable = true;
-  #meenzen.services.kener.enable = true;
-  #meenzen.services.conduit.enable = true;
-  #meenzen.services.miniflux.enable = true;
-  #meenzen.services.forgejo.enable = true;
+  meenzen.nginx = {
+    enable = true;
+    enableCloudflare = true;
+    testPage = "neon.mnzn.dev";
+  };
+  meenzen.grafana.enable = true;
+  meenzen.promtail.enable = true;
+  meenzen.postgresql.enable = true;
+  meenzen.oci-containers.enable = true;
+  meenzen.mastodon = {
+    enable = true;
+    enableSearch = true;
+  };
+  meenzen.fedifetcher.enable = true;
+  meenzen.matrix.enable = true;
+  meenzen.authentik.enable = true;
+  meenzen.gitlab.enable = true;
+  meenzen.collabora.enable = true;
+  meenzen.cheshbot.enable = true;
+  meenzen.attic.enable = true;
+  meenzen.distributed-build.enableHost = true;
+  meenzen.mudblazor-docs.enable = true;
+  meenzen.websites."mnzn.dev".enable = true;
+  meenzen.services.kener.enable = true;
+  meenzen.services.conduit.enable = true;
+  meenzen.services.miniflux.enable = true;
+  meenzen.services.forgejo.enable = true;
 }
