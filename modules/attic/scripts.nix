b6fe4e64d40a778a5196765c8982ddeb0eb6d81f
Samuel Meenzen <samuel@meenzen.net>
2025-09-15 10:30:02 +0200

    feat(attic): use writeShellApplication for scripts

--- modules/attic/scripts.nix
+++ modules/attic/scripts.nix
@@ -2,37 +2,45 @@
   environment.systemPackages = [
     pkgs.attic-client
     (
-      pkgs.writeScriptBin "attic-login" ''
-        set -e
-        if [ -z "$1" ]; then
-          echo "Usage: $0 <token>"
-          exit 1
-        fi
-        attic login meenzen https://attic.mnzn.dev $@
-      ''
+      pkgs.writeShellApplication {
+        name = "attic-login";
+        text = ''
+          if [ -z "$1" ]; then
+            echo "Usage: $0 <token>"
+            exit 1
+          fi
+          attic login meenzen https://attic.mnzn.dev "$@"
+        '';
+      }
     )
     (
-      pkgs.writeScriptBin "attic-push-path" ''
-        set -e
-        if [ -z "$1" ]; then
-          echo "Usage: $0 <path>"
-          echo "Example: $0 \$(which bash)"
-          exit 1
-        fi
-        attic push meenzen:main $@
-      ''
+      pkgs.writeShellApplication {
+        name = "attic-push-path";
+        text = ''
+          if [ -z "$1" ]; then
+            echo "Usage: $0 <path>"
+            echo "Example: $0 \$(which bash)"
+            exit 1
+          fi
+          attic push meenzen:main "$@"
+        '';
+      }
     )
     (
-      pkgs.writeScriptBin "attic-push-system" ''
-        set -eux
-        attic push meenzen:main /run/current-system $@
-      ''
+      pkgs.writeShellApplication {
+        name = "attic-push-system";
+        text = ''
+          attic push meenzen:main /run/current-system "$@"
+        '';
+      }
     )
     (
-      pkgs.writeScriptBin "attic-push-everything" ''
-        set -e
-        nix path-info --all | ${pkgs.gnugrep}/bin/grep --invert '\.drv$' | attic push meenzen:main --stdin $@
-      ''
+      pkgs.writeShellApplication {
+        name = "attic-push-everything";
+        text = ''
+          nix path-info --all | ${pkgs.gnugrep}/bin/grep --invert '\.drv$' | attic push meenzen:main --stdin "$@"
+        '';
+      }
     )
   ];
 }
