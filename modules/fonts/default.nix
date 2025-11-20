791a1da48c2154e01848ddafeb424364cf9e1587
Samuel Meenzen <samuel@meenzen.net>
2025-11-20 09:56:34 +0100

    chore: update font config

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -9,60 +9,62 @@
 in {
   options.meenzen.fonts = {
     enable = lib.mkEnableOption "Add Fonts";
+    enableFlatpakFix = lib.mkEnableOption "Enable Flatpak font fix";
   };
 
-  config = lib.mkIf cfg.enable {
-    # Fonts
-    # system.fsPackages = [pkgs.bindfs];
-    # fileSystems = let
-    #   mkRoSymBind = path: {
-    #     device = path;
-    #     fsType = "fuse.bindfs";
-    #     options = ["ro" "resolve-symlinks" "x-gvfs-hide"];
-    #   };
-    #   aggregated = pkgs.buildEnv {
-    #     name = "system-fonts-and-icons";
-    #     paths =
-    #       config.fonts.packages
-    #       ++ [
-    #         #pkgs.kdePackages.breeze
-    #         #pkgs.gnome-themes-extra
-    #       ];
-    #     pathsToLink = ["/share/fonts" "/share/icons"];
-    #   };
-    # in {
-    #   "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
-    #   "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
-    #};
+  config =
+    lib.mkIf cfg.enable {
+      fonts = {
+        fontDir.enable = true;
+        packages = [
+          pkgs.nerd-fonts.fira-code
+          pkgs.nerd-fonts.hack
+          pkgs.nerd-fonts.jetbrains-mono
 
-    fonts = {
-      fontDir.enable = true;
-      packages = [
-        pkgs.nerd-fonts.fira-code
-        pkgs.nerd-fonts.hack
-        pkgs.nerd-fonts.jetbrains-mono
+          # default fonts
+          pkgs.dejavu_fonts
+          pkgs.freefont_ttf
+          pkgs.gyre-fonts # TrueType substitutes for standard PostScript fonts
+          pkgs.liberation_ttf
+          pkgs.unifont
+          pkgs.noto-fonts
+          pkgs.noto-fonts-cjk-sans
+          pkgs.noto-fonts-color-emoji
 
-        # default fonts
-        pkgs.dejavu_fonts
-        pkgs.freefont_ttf
-        pkgs.gyre-fonts # TrueType substitutes for standard PostScript fonts
-        pkgs.liberation_ttf
-        pkgs.unifont
-        pkgs.noto-fonts
-        pkgs.noto-fonts-cjk-sans
-        pkgs.noto-fonts-color-emoji
-
-        # microsoft fonts
-        pkgs.corefonts
-        pkgs.vista-fonts
-      ];
-      fontconfig = {
-        antialias = true;
-        cache32Bit = true;
-        hinting.enable = true;
-        hinting.autohint = true;
-        useEmbeddedBitmaps = true;
+          # microsoft fonts
+          pkgs.corefonts
+          pkgs.vista-fonts
+        ];
+        fontconfig = {
+          antialias = true;
+          cache32Bit = true;
+          hinting.enable = true;
+          hinting.autohint = true;
+          useEmbeddedBitmaps = true;
+        };
+      };
+    }
+    // lib.mkIf cfg.enableFlatpakFix {
+      system.fsPackages = [pkgs.bindfs];
+      fileSystems = let
+        mkRoSymBind = path: {
+          device = path;
+          fsType = "fuse.bindfs";
+          options = ["ro" "resolve-symlinks" "x-gvfs-hide"];
+        };
+        aggregated = pkgs.buildEnv {
+          name = "system-fonts-and-icons";
+          paths =
+            config.fonts.packages
+            ++ [
+              #pkgs.kdePackages.breeze
+              #pkgs.gnome-themes-extra
+            ];
+          pathsToLink = ["/share/fonts" "/share/icons"];
+        };
+      in {
+        "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
+        "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
       };
     };
-  };
 }
