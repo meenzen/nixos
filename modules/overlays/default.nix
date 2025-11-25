c5759e33c44b07ffa5f2e916119aca02aeb5421a
Samuel Meenzen <samuel@meenzen.net>
2025-11-25 23:40:08 +0100

    chore: update

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -4,22 +4,5 @@
   ...
 }: {
   nixpkgs.overlays = [
-    # tokei: use the latest alpha since there hasn't been a stable release since 2021
-    # https://github.com/XAMPPRocky/tokei/issues/911
-    (final: prev: {
-      tokei = prev.tokei.overrideAttrs (oldAttrs: rec {
-        version = "13.0.0-alpha.8";
-        src = prev.fetchFromGitHub {
-          owner = "XAMPPRocky";
-          repo = "tokei";
-          rev = "v${version}";
-          sha256 = "sha256-jCI9VM3y76RI65E5UGuAPuPkDRTMyi+ydx64JWHcGfE=";
-        };
-        cargoDeps = final.rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-LzlyrKaRjUo6JnVLQnHidtI4OWa+GrhAc4D8RkL+nmQ=";
-        };
-      });
-    })
   ];
 }
