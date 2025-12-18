b11484e5cccac700f11393b77e12a374af4b9135
Samuel Meenzen <samuel@meenzen.net>
2025-12-18 19:02:20 +0100

    chore: update dotnet config

--- home-manager/modules/dev-tools/default.nix
+++ home-manager/modules/dev-tools/default.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   inputs,
   ...
 }: let
@@ -8,11 +9,31 @@
     pkgs.dotnetCorePackages.sdk_9_0
     pkgs.dotnetCorePackages.sdk_8_0
   ];
+  dotnetRoot = "${dotnet}/share/dotnet";
 in {
   home.sessionVariables = {
-    DOTNET_ROOT = "${dotnet}/share/dotnet";
-    MSBUILDTERMINALLOGGER = "auto";
+    DOTNET_ROOT = dotnetRoot;
+    DOTNET_ROOT_X64 = dotnetRoot;
+
+    # General reliability
+    DOTNET_MULTILEVEL_LOOKUP = "0";
+    DOTNET_CLI_HOME = "$HOME/.dotnet";
+    NUGET_PACKAGES = "$HOME/.nuget/packages";
+
+    # Shut up, Microsoft
+    DOTNET_NOLOGO = "1";
+    DOTNET_CLI_TELEMETRY_OPTOUT = "1";
   };
+
+  # Just to be sure
+  home.activation.ensureDotnetDirs = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p \
+      "$HOME/.dotnet" \
+      "$HOME/.nuget/packages" \
+      "$HOME/.nuget/NuGet" \
+      "$HOME/.local/share/NuGet"
+  '';
+
   home.packages = [
     # Compilers
     dotnet
@@ -28,6 +49,7 @@ in {
     pkgs.gh
     pkgs.glab
     pkgs.nixpkgs-review
+    pkgs.csharprepl
 
     # Kubernetes
     pkgs.kubectl
