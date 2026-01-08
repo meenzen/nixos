224620775a05740e01bcd1187b22002c1334b4ef
Samuel Meenzen <samuel@meenzen.net>
2026-01-08 08:51:55 +0100

    chore: update

--- systems/framework/configuration.nix
+++ systems/framework/configuration.nix
@@ -17,7 +17,7 @@
   networking.firewall.enable = true;
   boot.initrd.luks.devices."luks-d72b6916-393c-4db9-8194-6d48d1cf5189".device = "/dev/disk/by-uuid/d72b6916-393c-4db9-8194-6d48d1cf5189";
 
-  meenzen.distributed-build.enable = true;
+  meenzen.distributed-build.enable = false;
   meenzen.desktop.enable = true;
   meenzen.plasma.tiling.enable = true;
   meenzen.hyprland.enable = false;
