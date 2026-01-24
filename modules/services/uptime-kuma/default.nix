7d7e7cadba50d28443c024b80bcf698c71108e1c
Samuel Meenzen <samuel@meenzen.net>
2026-01-24 23:15:40 +0100

    feat: more status pages

--- modules/services/uptime-kuma/default.nix
+++ modules/services/uptime-kuma/default.nix
@@ -5,13 +5,32 @@
   ...
 }: let
   cfg = config.meenzen.services.uptime-kuma;
+  domains = [cfg.domain] ++ cfg.extraDomains;
+  virtualHosts = builtins.listToAttrs (map (domain: {
+      name = domain;
+      value = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString cfg.port}";
+        };
+      };
+    })
+    domains);
 in {
   options.meenzen.services.uptime-kuma = {
     enable = lib.mkEnableOption "Enable Uptime Kuma";
     domain = lib.mkOption {
       type = lib.types.str;
-      default = "status.mnzn.dev";
+      default = "uptime.mnzn.dev";
       description = "Domain for Uptime Kuma";
+    };
+    extraDomains = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "status.mnzn.dev"
+      ];
+      description = "Additional domains for Uptime Kuma";
     };
     port = lib.mkOption {
       type = lib.types.int;
@@ -30,12 +49,6 @@ in {
 
     meenzen.backup.paths = [config.services.uptime-kuma.settings.DATA_DIR];
 
-    services.nginx.virtualHosts.${cfg.domain} = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString cfg.port}";
-      };
-    };
+    services.nginx.virtualHosts = virtualHosts;
   };
 }
