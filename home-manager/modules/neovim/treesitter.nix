de591cdb179e079dccb4e5da2f4cf36ba80b08d9
Samuel Meenzen <samuel@meenzen.net>
2026-01-24 15:39:53 +0100

    chore: update

--- home-manager/modules/neovim/treesitter.nix
+++ home-manager/modules/neovim/treesitter.nix
@@ -5,7 +5,6 @@
       settings.indent.enable = true;
     };
     treesitter-context.enable = true;
-    treesitter-refactor.enable = true;
     treesitter-textobjects.enable = true;
     ts-context-commentstring.enable = true;
   };
