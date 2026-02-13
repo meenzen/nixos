6f72c6ec4e09e99188658d845937381a172264d6
Samuel Meenzen <samuel@meenzen.net>
2026-02-13 11:09:31 +0100

    chore: update

--- modules/latest-kernel/default.nix
+++ modules/latest-kernel/default.nix
@@ -15,7 +15,6 @@ in {
     boot.kernelPackages =
       if cfg.enable
       then pkgs.linuxPackages_latest
-      # todo: set to pkgs.linuxPackages when https://github.com/NixOS/nixpkgs/pull/485549 is merged
-      else pkgs.linuxPackages_6_18;
+      else pkgs.linuxPackages;
   };
 }
