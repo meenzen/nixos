14ee054ab9a2017399daeb34f3edb96e8aaa1a23
Samuel Meenzen <samuel@meenzen.net>
2026-04-01 14:04:58 +0200

    feat: flip.mnzn.dev website

--- modules/services/minecraft/flip.nix
+++ modules/services/minecraft/flip.nix
@@ -6,9 +6,29 @@
   ...
 }: let
   cfg = config.meenzen.services.minecraft.flip;
+  modpack = pkgs.fetchPackwizModpack {
+    url = "https://forge.mnzn.dev/FLiP/minecraft-modpack/raw/commit/d5ca807b4612a2ce285db6d39f7929eaed0d2417/pack.toml";
+    packHash = "sha256-lsUuqgfQrZl2+4Zsy1tb1h8aYKnwZDgIjjHdomuzNZI=";
+    manifestHash = "sha256:04ndbdd6bfh2zja9vdvi33lp9gpsnqbfnpfwfx6a8scs1kwwn39b";
+  };
+  mcVersion = modpack.manifest.versions.minecraft;
+  fabricVersion = modpack.manifest.versions.fabric;
+  serverVersion = lib.replaceStrings ["."] ["_"] "fabric-${mcVersion}";
+  website = pkgs.runCommand "build-flip-website" {} ''
+    mkdir -p $out
+    cp -r ${./flip.mnzn.dev}/* $out/
+    # replace the placeholder in the HTML with the actual domain
+    sed -i "s/{{DOMAIN}}/${cfg.domain}/g" $out/index.html
+    sed -i "s/{{JAVA_EDITION_VERSION}}/${mcVersion}/g" $out/index.html
+  '';
 in {
   options.meenzen.services.minecraft.flip = {
     enable = lib.mkEnableOption "Enable FLiP Minecraft server";
+    domain = lib.mkOption {
+      type = lib.types.str;
+      default = "flip.mnzn.dev";
+      description = "Domain for the FLiP Minecraft server";
+    };
     voiceChatPort = lib.mkOption {
       type = lib.types.int;
       default = 24454;
@@ -26,16 +46,7 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.minecraft-servers.servers.flip = let
-      modpack = pkgs.fetchPackwizModpack {
-        url = "https://forge.mnzn.dev/FLiP/minecraft-modpack/raw/commit/d5ca807b4612a2ce285db6d39f7929eaed0d2417/pack.toml";
-        packHash = "sha256-lsUuqgfQrZl2+4Zsy1tb1h8aYKnwZDgIjjHdomuzNZI=";
-        manifestHash = "sha256:04ndbdd6bfh2zja9vdvi33lp9gpsnqbfnpfwfx6a8scs1kwwn39b";
-      };
-      mcVersion = modpack.manifest.versions.minecraft;
-      fabricVersion = modpack.manifest.versions.fabric;
-      serverVersion = lib.replaceStrings ["."] ["_"] "fabric-${mcVersion}";
-    in {
+    services.minecraft-servers.servers.flip = {
       enable = true;
       jvmOpts = "-Xmx8G -Xms2G";
       package = pkgs.fabricServers.${serverVersion}.override {loaderVersion = fabricVersion;};
@@ -63,5 +74,11 @@ in {
         }
       )
     ];
+
+    services.nginx.virtualHosts.${cfg.domain} = {
+      enableACME = true;
+      forceSSL = true;
+      root = website;
+    };
   };
 }
