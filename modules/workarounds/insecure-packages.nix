29399d78dfa166840425fcacedc8c2202d760cc4
Samuel Meenzen <samuel@meenzen.net>
2026-06-30 11:11:32 +0200

    chore: update

--- modules/workarounds/insecure-packages.nix
+++ modules/workarounds/insecure-packages.nix
@@ -1,3 +1,6 @@
 {
-  nixpkgs.config.permittedInsecurePackages = [];
+  nixpkgs.config.permittedInsecurePackages = [
+    # Temporary workaround, remove this soon: https://github.com/NixOS/nixpkgs/issues/535580#issuecomment-4809489104
+    "pnpm-10.29.2"
+  ];
 }
