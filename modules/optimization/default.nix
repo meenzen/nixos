03bcb767bd629c297e8fecc7d67e561ac4e1edc5
Samuel Meenzen <samuel@meenzen.net>
2026-07-04 00:36:04 +0200

    feat(journal): keep less logs

--- modules/optimization/default.nix
+++ modules/optimization/default.nix
@@ -3,4 +3,10 @@
   boot.kernel.sysctl = {
     "vm.swappiness" = 60;
   };
+
+  # Don't keep too many logs around
+  services.journald.extraConfig = ''
+    MaxRetentionSec=7day
+    SystemMaxUse=1G
+  '';
 }
