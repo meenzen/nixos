2dabd29b6a12a7b6e6a543b6292c60b38b262b88
Samuel Meenzen <samuel@meenzen.net>
2026-07-06 15:37:07 +0200

    feat: ad openfortivpn script

--- modules/openfortivpn/default.nix
+++ modules/openfortivpn/default.nix
@@ -14,6 +14,18 @@ in {
     environment.systemPackages = [
       pkgs.openfortivpn
       pkgs.openfortivpn-webview
+      (
+        pkgs.writeShellApplication
+        {
+          name = "openfortivpn-connect";
+          text = ''
+            VPN_HOST=vpn.human.de
+            VPN_PORT=443
+            openfortivpn-webview "''${VPN_HOST}:''${VPN_PORT}" 2>/dev/null \
+            | sudo openfortivpn "''${VPN_HOST}:''${VPN_PORT}" --cookie-on-stdin --pppd-accept-remote
+          '';
+        }
+      )
     ];
   };
 }
