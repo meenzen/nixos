d0dc8b74d5a8d46047bf20487fd4604d4422840a
Samuel Meenzen <samuel@meenzen.net>
2026-07-06 09:52:23 +0200

    chore: remove insecure pacakge override

--- modules/workarounds/insecure-packages.nix
+++ modules/workarounds/insecure-packages.nix
@@ -1,6 +1,4 @@
 {
   nixpkgs.config.permittedInsecurePackages = [
-    # Temporary workaround, remove this soon: https://github.com/NixOS/nixpkgs/issues/535580#issuecomment-4809489104
-    "pnpm-10.29.2"
   ];
 }
