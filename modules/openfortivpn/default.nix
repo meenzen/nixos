b276bc0780ce3c74239386ae646bdaa8093ebd0d
Samuel Meenzen <samuel@meenzen.net>
2026-07-13 12:12:12 +0200

    feat: use upstream openfortivpn-webview-qt package

--- modules/openfortivpn/default.nix
+++ modules/openfortivpn/default.nix
@@ -5,28 +5,12 @@
   ...
 }: let
   cfg = config.meenzen.openfortivpn;
-  # Update request: https://github.com/NixOS/nixpkgs/issues/539205
-  openfortivpn-webview-qt = pkgs.openfortivpn-webview-qt.overrideAttrs (_: rec {
-    version = "1.3.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "gm-vm";
-      repo = "openfortivpn-webview";
-      rev = "v${version}-qt";
-      hash = "sha256-TohrOgLzvxmUsRVV36XHgE9ul38CjU/qKF+LZOZQieE=";
-    };
-    sourceRoot = "${src.name}/openfortivpn-webview-qt";
-    nativeBuildInputs = [
-      pkgs.qt6Packages.wrapQtAppsHook
-      pkgs.cmake
-      pkgs.ninja
-    ];
-    buildInputs = [
-      pkgs.qt6Packages.qtbase
-      pkgs.qt6Packages.qtwebengine
-    ];
-    patches = [
-      ./pin-dialog.patch
-    ];
+  openfortivpn-webview-qt = pkgs.openfortivpn-webview-qt.overrideAttrs (oldAttrs: {
+    patches =
+      (oldAttrs.patches or [])
+      ++ [
+        ./pin-dialog.patch
+      ];
   });
 in {
   options.meenzen.openfortivpn = {
