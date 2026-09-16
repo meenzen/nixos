2908703d2ea7be2667d05b9d68b529bc0fd821cd
Samuel Meenzen <samuel@meenzen.net>
2026-09-16 09:50:41 +0200

    feat(lauti): add caching

--- modules/services/lauti/default.nix
+++ modules/services/lauti/default.nix
@@ -64,6 +64,13 @@ in {
           proxyPass = "http://127.0.0.1:${toString cfg.port}";
           proxyWebsockets = true;
         };
+        locations."~* \\.(?:css|js|mjs|png|jpe?g|gif|svg|webp|ico|woff2?|ttf)$" = {
+          proxyPass = "http://127.0.0.1:${toString cfg.port}";
+          extraConfig = ''
+            proxy_hide_header Cache-Control;
+            add_header Cache-Control "public, max-age=86400" always;
+          '';
+        };
         locations."^~ /google" = {
           root = ./webroot;
         };
