e139ef866d6f952a0a0f47f039aea145fb5f9a7f
Samuel Meenzen <samuel@meenzen.net>
2026-09-25 19:25:08 +0200

    fix: patch gamescope for nvidia

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -1,4 +1,15 @@
 _: {
   nixpkgs.overlays = [
+    (_: prev: {
+      gamescope = prev.gamescope.overrideAttrs (old: {
+        patches =
+          (old.patches or [])
+          ++ [
+            # Fixes artifacting on NVIDIA by forcing contiguous scanout allocations via GBM
+            # https://github.com/antheas/gamescope/commit/fa6f7f2503c2e773acf818ad688ddd1db73df3a0
+            ./gamescope-nvidia-scanout.patch
+          ];
+      });
+    })
   ];
 }
